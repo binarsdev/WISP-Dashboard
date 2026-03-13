8beb4a14996237bec64511bb893b238559f6c195
Carlos López Barrera <52845915+binarsdev@users.noreply.github.com>
2026-03-13 16:11:32 -0600

    Add files via upload
    Parte 1 de los Archivos

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<shalom','mikhmon>|>amFkbQ==');
+
+$data['OJITOS'] = array ('1'=>'OJITOS!10.5.50.1','OJITOS@|@carlos','OJITOS#|#nJaoamFibA==','OJITOS%WIFI SH@LOM','OJITOS^shalomlogin.net','OJITOS&$','OJITOS*10','OJITOS(1','OJITOS)','OJITOS=10','OJITOS@!@enable');
+$data['CHR'] = array ('1'=>'CHR!216.238.68.119','CHR@|@carlos','CHR#|#nJaoamFibA==','CHR%CHR','CHR^216.238.68.119','CHR&Rp','CHR*10','CHR(1','CHR)','CHR=10','CHR@!@disable');
+$data['ZOTO'] = array ('1'=>'ZOTO!192.168.5.1','ZOTO@|@carlos','ZOTO#|#nJaoamFibA==','ZOTO%SH@LOM WIFI','ZOTO^login.fichas.com','ZOTO&$','ZOTO*10','ZOTO(1','ZOTO)','ZOTO=10','ZOTO@!@enable');
+$data['HUEYITLALPAN'] = array ('1'=>'HUEYITLALPAN!192.168.0.3','HUEYITLALPAN@|@isaac','HUEYITLALPAN#|#j5qempakeHSTqp2hq356','HUEYITLALPAN%WIFI SH@LOM','HUEYITLALPAN^login.mired.com','HUEYITLALPAN&$','HUEYITLALPAN*10','HUEYITLALPAN(1','HUEYITLALPAN)','HUEYITLALPAN=10','HUEYITLALPAN@!@disable');
